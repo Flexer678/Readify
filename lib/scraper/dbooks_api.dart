bf70bbb6885c8e3e66249834a32c22478990178e
Flexer678 <81823862+Flexer678@users.noreply.github.com>
2023-09-11 22:06:43 -0500

    stuff added2

--- lib/scraper/dbooks_api.dart
+++ lib/scraper/dbooks_api.dart
@@ -3,19 +3,6 @@ import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
-<<<<<<< Updated upstream
-class dbooks{
-
-  final String url = "https://www.dbooks.org/api/"
-  Future getSearch(String title) async {
-  try{
-    final uri = Uri.parse("${url}search/${title}");
-  final response = await http.get(uri);
-  if (response.statusCode == 200) {
-    return response.body;
-  } else {
-    return [];
-=======
 class dbooks {
   final String url = "https://www.dbooks.org/api/";
   Future getSearch(String title) async {
@@ -44,24 +31,5 @@ class dbooks {
     } catch (e) {
       return [];
     }
->>>>>>> Stashed changes
   }
-  }catch(e){
-    return []
-  }
-}
-  Future getDetails(String title) async {
-    try{
-      final uri = Uri.parse("book/${title}");
-      final response = await http.get(uri);
-    if (response.statusCode == 200) {
-      return response.body;
-    } else {
-      return [];
-    }
-    }catch(e){
-      return []
-    }
-}
-
 }
