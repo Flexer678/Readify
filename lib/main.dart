f95f5e2b1485e9ea41d7b57a39bd7b19e3382b8a
Flexer678 <81823862+Flexer678@users.noreply.github.com>
2023-10-25 18:10:41 -0500

    a few things addded

--- lib/main.dart
+++ lib/main.dart
@@ -3,16 +3,11 @@ import 'package:provider/provider.dart';
 import 'package:readify/pages/mainpage.dart';
 import 'package:readify/provider/notification_provider.dart';
 import 'package:readify/provider/provider.dart';
-import 'package:hive/hive.dart';
-import 'package:hive_flutter/hive_flutter.dart';
 
-late Box box;
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await NotificationProvider().initNotification();
-  await Hive.initFlutter();
-  box = await Hive.openBox('myList');
-  //Hive.registerAdapter(MyObjectAdapter());
+
   runApp(MultiProvider(
     providers: [ChangeNotifierProvider(create: (_) => ProviderHandler())],
     child: MyApp(),
